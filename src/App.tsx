7edf5d25fd02a75b925c8c8c477358882d91129a
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-04-08 09:48:47 +0800

    Remove random margin

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import Introduction from './components/Introduction';
-import Sidebar from './components/sidebar/Sidebar';
 import { Col, Container, Row } from 'react-bootstrap';
+import Sidebar from './components/sidebar/Sidebar';
+import NavigationBar from './components/mobile-navbar/NavigationBar';
+import Introduction from './components/Introduction';
 import AboutMe from './components/aboutme/AboutMe';
 import Projects from './components/projects/Projects';
 import Experiences from './components/experiences/Experiences';
-import NavigationBar from './components/mobile-navbar/NavigationBar';
 import ContactUs from './components/contact/Email';
 
 function App() {
   return (
-    <div className='bg-dark text-light'>
-      <Container fluid className='m-0 p-0'>
-        <Row className='vh-100'>
-          <Sidebar />
-          <NavigationBar />
-          <Col className='main-body'>
-            <Row id='introduction'>
-              <Introduction />
-            </Row>
-            <Row id='aboutMe'>
-              <AboutMe />
-            </Row>
-            <Row id='projects'>
-              <Projects />
-            </Row>
-            <Row id='experiences'>
-              <Experiences />
-            </Row>
-            <Row id='contact'>
-              <ContactUs />
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Container fluid className='m-0 p-0'>
+      <Row className='vh-100 g-0'>
+        <NavigationBar />
+        <Sidebar />
+        <Col className='main-body'>
+          <Row id='introduction' className='g-0'>
+            <Introduction />
+          </Row>
+          <Row id='aboutMe' className='g-0'>
+            <AboutMe />
+          </Row>
+          <Row id='projects' className='g-0'>
+            <Projects />
+          </Row>
+          <Row id='experiences' className='g-0'>
+            <Experiences />
+          </Row>
+          <Row id='contact' className='g-0'>
+            <ContactUs />
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
